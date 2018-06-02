63d2a63a9cf38e4dbe363b768d7a77c13dd2e57a
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-02 02:58:22 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,13 @@
 def hello_t(array)
-  i = 0
+  if block_given?
+    i = 0
  
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+  else
+    
   array
 end
 
