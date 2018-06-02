0fb2a7a920aad5c4f204622ccbcc0bf0bfbc513c
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-02 02:45:31 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,9 @@
 def hello_t(array)
-  hello_t(["Tim", "Tom", "Jim"]) do |name|
+  
+endhello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
-end
 
 end
 
